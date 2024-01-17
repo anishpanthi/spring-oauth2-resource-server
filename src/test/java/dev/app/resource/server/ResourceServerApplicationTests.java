0e66d1e7a60d46cc98f884ab944ff949da3c58d7
Panthi <anish.panthi41@gmail.com>
2024-01-17 16:25:30 -0600

    Initial Commit

---
+++ src/test/java/dev/app/resource/server/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.app.resource.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
