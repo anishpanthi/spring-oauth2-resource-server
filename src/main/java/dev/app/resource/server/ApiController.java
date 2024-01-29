6e7961af7eb2accd91eb9cf35745786a9e68e930
Panthi <anish.panthi41@gmail.com>
2024-01-29 17:31:24 -0600

    Added B2C login flow

--- src/main/java/dev/app/resource/server/ApiController.java
+++ src/main/java/dev/app/resource/server/ApiController.java
@@ -1,18 +1,27 @@
 package dev.app.resource.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Anish Panthi
  */
 @RestController
+@Log4j2
 public class ApiController {
 
   @GetMapping("/api/greet")
   public Map<String, String> greet() {
+    return Map.of(
+        "message", "Hello World! Welcome to Java Peer Group Session: Season 2, Episode 1");
+  }
+
+  @GetMapping("/api/secure")
+  public Map<String, String> secure(@PathVariable String token) {
+    log.info("Token: {}", token);
     return Map.of(
         "message", "Hello World! Welcome to Java Peer Group Session: Season 2, Episode 1");
   }
