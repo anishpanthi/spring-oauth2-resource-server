fe04ebd788f146e41efc2ac0547bdeccfb9a0199
Panthi <anish.panthi41@gmail.com>
2024-02-06 10:24:15 -0600

    WIP... Refactored classes and properties

--- src/main/java/dev/app/resource/server/ApiController.java
+++ src/main/java/dev/app/resource/server/ApiController.java
@@ -2,8 +2,9 @@ package dev.app.resource.server;
 
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,10 +20,13 @@ public class ApiController {
         "message", "Hello World! Welcome to Java Peer Group Session: Season 2, Episode 1");
   }
 
-  @GetMapping("/api/secure")
-  public Map<String, String> secure(@PathVariable String token) {
-    log.info("Token: {}", token);
+  @GetMapping
+  public Map<String, Object> secure(@AuthenticationPrincipal OidcUser oidcUser) {
+    log.info("User: {}", oidcUser);
     return Map.of(
-        "message", "Hello World! Welcome to Java Peer Group Session: Season 2, Episode 1");
+        "message",
+        "Hello World! Welcome to Java Peer Group Session: Season 2, Episode 2",
+        "claims",
+        oidcUser.getClaims());
   }
 }
